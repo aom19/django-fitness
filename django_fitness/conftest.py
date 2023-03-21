251d5b8b3e4afdac92e7f7a28a7fd7867454dfb2
aom19 <39848284+aom19@users.noreply.github.com>
2023-03-21 22:26:21 +0200

    init repo

---
+++ django_fitness/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_fitness.users.models import User
+from django_fitness.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
